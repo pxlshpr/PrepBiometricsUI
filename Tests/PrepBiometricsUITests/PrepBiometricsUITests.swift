9e37733016cb6831f1b8138d26e7d71ab8842ab6
Ahmed Khalaf <pxlshpr@gmail.com>
2023-10-28 14:31:27 +0500

    Initial Commit

---
+++ Tests/PrepBiometricsUITests/PrepBiometricsUITests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import PrepBiometricsUI
+
+final class PrepBiometricsUITests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
